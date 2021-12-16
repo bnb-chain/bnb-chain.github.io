828106767fa1a5af1a1d36e132d60bb9e600b2c8
RumeelHussainbnb <93580180+RumeelHussainbnb@users.noreply.github.com>
2021-12-16 21:27:34 +0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -66,7 +66,7 @@ const config = {
         style: 'primary',
         logo: {
           alt: 'BSC',
-          src: 'img/logo.svg',
+          src: 'img/image.png',
         },
         items: [
           {
