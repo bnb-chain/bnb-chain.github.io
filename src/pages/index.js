bcc71405e5f746aa0a5700f3f69865bf8d8003ad
rumeelhussainbnb <rumeelhussainbnb@gmail.com>
2022-03-15 12:15:49 +0500

    bnb-update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,4 @@
 import React from 'react';
-/*import clsx from 'clsx';
-import style from '../css/custom.css'
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
-import CardPrint from '../components/HomepageFeatures';*/
 import  { Redirect } from 'react-router-dom';
 
 export default function Home() {
