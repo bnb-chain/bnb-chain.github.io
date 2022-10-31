d6d7b46c57caf4de9ba87fc8ddce87791c8f2251
RumeelHussainbnb <rumeelhussainbnb@gmail.com>
2022-10-31 16:35:47 +0500

    Added GTM Configuration

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,12 +19,14 @@ const config = {
   
   plugins: [
              require.resolve("docusaurus-plugin-image-zoom"),
-             require.resolve('docusaurus-gtm-plugin'),
+              
+              [require.resolve('docusaurus-gtm-plugin'),
               {
-                id: 'GTM-W9BVQXM',
-              }
-           ],
-  
+                  id: 'GTM-W9BVQXM', // GTM Container ID
+              }],
+              
+            ],
+
   presets: [
     [
       '@docusaurus/preset-classic',
