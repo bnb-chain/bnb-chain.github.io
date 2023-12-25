a3dd845dcd41a7a768a91c329cf7006a14eef3a8
JakeLin1023 <105701320+JakeLin1023@users.noreply.github.com>
2023-12-25 14:01:50 +0800

    fix: Docrarus url (#250)
    * feat: Gtag
    
    * fix: Docusaurus url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'BNB Chain Documentation',
-  url: 'https://bnb-chain.github.io/',
+  url: 'https://docs.bnbchain.org',
   baseUrl: '/',
   onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
